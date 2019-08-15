3c0f9cb93adc13d98c5738d040afd079ec11be37
Rohan Dharwadker <RohanDhar@NB0011576.silabs.com>
2019-08-15 13:41:51 -0500

    fixed img for apps

--- theme.js
+++ theme.js
@@ -1,8 +1,8 @@
 var pIndex = 0;
 var pIndex2 = 0;
 if (document.title == "Home - WN") {
-    pIndex = 2;
-    pIndex2 = 2;
+    pIndex = 3;
+    pIndex2 = 3;
 }
 else if (document.title == "Website Builder - WN") {
     pIndex = 0;
@@ -12,17 +12,19 @@ else if (document.title == "Settings - WN") {
     pIndex = 0
     pIndex2 = 0
 }
+var bodytag = document.getElementById('bodytag');
+var nav_button_set = document.getElementById("nav_button_set");
+var nav_button_app = document.getElementById("nav_button_ap");
+var nav_button_wb = document.getElementById("nav_button_wb");
+var navbar = document.getElementById('navbar');
 var ptag = document.getElementsByTagName("P")[pIndex];
 var ptag2 = document.getElementsByTagName("P")[pIndex2];
-var navbar = document.getElementById('navbar');
-var nav_button_wb = document.getElementById("nav_button_wb");
 var nav_button_fb = document.getElementById("nav_button_fb");
 var nav_button_sw = document.getElementById("nav_button_sw");
-var nav_button_set = document.getElementById("nav_button_set");
 var main = document.getElementById('main');
 var main2 = document.getElementById('main2');
 var logo = document.getElementById('logo');
-var bodytag = document.getElementById('bodytag');   
+
 
 
 
@@ -31,27 +33,29 @@ var bodytag = document.getElementById('bodytag');
 function theme_load() {
     
     if (localStorage.theme == "light") {
+        bodytag.style.backgroundColor = 'white';
+        nav_button_set.style.backgroundColor = 'white';
+        nav_button_app.style.backgroundColor = 'white';
+        nav_button_wb.style.backgroundColor = 'white';
+        logo.style.backgroundColor = 'white';
         ptag.style.color = 'black';
         ptag2.style.color = 'black';
         navbar.style.backgroundColor = 'white';
-        logo.style.backgroundColor = 'white';
-        nav_button_set.style.backgroundColor = 'white';
         main.style.backgroundColor = '#ebebeb';
-        bodytag.style.backgroundColor = 'white';
-        nav_button_wb.style.backgroundColor = 'white';
         main2.style.backgroundColor = '#ebebeb';
         nav_button_fb.style.backgroundColor = 'white';
         nav_button_sw.style.backgroundColor = 'white';
     }
     else if (localStorage.theme == "dark") {
+        bodytag.style.backgroundColor = 'black';
+        nav_button_set.style.backgroundColor = 'black';
+        nav_button_app.style.backgroundColor = 'black';
+        nav_button_wb.style.backgroundColor = 'black';
+        logo.style.backgroundColor = 'black';
         ptag.style.color = 'grey';
         ptag2.style.color = 'grey';
         navbar.style.backgroundColor = 'black';
-        logo.style.backgroundColor = 'black';
-        nav_button_set.style.backgroundColor = 'black';
         main.style.backgroundColor = '#303030';
-        bodytag.style.backgroundColor = 'black';
-        nav_button_wb.style.backgroundColor = 'black';
         main2.style.backgroundColor = '#303030';
         nav_button_fb.style.backgroundColor = 'black';
         nav_button_sw.style.backgroundColor = 'black';
@@ -64,14 +68,18 @@ function theme_load() {
 }
 
 function theme_dark() {
+    bodytag.style.backgroundColor = 'black';
+    nav_button_app.style.backgroundColor = 'black';
+    nav_button_set.style.backgroundColor = 'black';
+    nav_button_wb.style.backgroundColor = 'black';
+    logo.style.backgroundColor = 'black';
     localStorage.theme = "dark";
     ptag.style.color = 'grey';
     ptag2.style.color = 'grey';
     navbar.style.backgroundColor = 'black';
-    logo.style.backgroundColor = 'black';
+    nav_button_app.style.backgroundColor = 'black';
     nav_button_set.style.backgroundColor = 'black';
     main.style.backgroundColor = '#303030';
-    bodytag.style.backgroundColor = 'black';
     nav_button_wb.style.backgroundColor = 'black';
     main2.style.backgroundColor = '#303030';
     nav_button_fb.style.backgroundColor = 'black';
@@ -79,14 +87,18 @@ function theme_dark() {
     
 }
 function theme_light() {
+    bodytag.style.backgroundColor = 'white';
+    nav_button_set.style.backgroundColor = 'white';
+    nav_button_app.style.backgroundColor = 'white';
+    nav_button_wb.style.backgroundColor = 'white';
+    logo.style.backgroundColor = 'white';
     localStorage.theme = "light";
     ptag.style.color = 'black';
     ptag2.style.color = 'black';
     navbar.style.backgroundColor = 'white';
-    logo.style.backgroundColor = 'white';
     nav_button_set.style.backgroundColor = 'white';
+    nav_button_app.style.backgroundColor = 'white';
     main.style.backgroundColor = '#ebebeb';
-    bodytag.style.backgroundColor = 'white';
     nav_button_wb.style.backgroundColor = 'white';
     main2.style.backgroundColor = '#ebebeb';
     nav_button_fb.style.backgroundColor = 'white';
