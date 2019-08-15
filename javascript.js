3e026f8bde8d1804fffa60a8fb9e10d65d6731ba
Rohan Dharwadker <RohanDhar@NB0011576.silabs.com>
2019-08-14 19:25:24 -0500

    added apps, updated styles

--- javascript.js
+++ javascript.js
@@ -20,12 +20,14 @@ window.onscroll = function() {
     var nav_button_fb = document.getElementById("nav_button_fb")
     var nav_button_sw = document.getElementById("nav_button_sw")
     var nav_button_set = document.getElementById("nav_button_set")
+    var nav_button_app = document.getElementById("nav_button_ap")
     if ( window.pageYOffset > 100 ) {
         nav.classList.add("links1");
         nav_button_wb.classList.add("links1")
         nav_button_fb.classList.add("links1")
         nav_button_sw.classList.add("links1")
         nav_button_set.classList.add("links1")
+        nav_button_app.classList.add("links1")
         // var nav_button_array = document.getElementsByClassName("dropbtn");
         // for(var i = 0; i < nav_button_array.length; i++)
         // {
@@ -37,6 +39,7 @@ window.onscroll = function() {
         nav_button_fb.classList.remove("links1")
         nav_button_sw.classList.remove("links1")
         nav_button_set.classList.remove("links1")
+        nav_button_app.classList.remove("links1")
         // for(var i = 0; i < nav_button_array.length; i++)
         // {
         //     nav_button_array[i].className -= "links1";
