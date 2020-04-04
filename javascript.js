170d98e8ffcdf1bf7359dd1f93221eb5ba4b277d
RawBaconOnAStick <rohan.dharwadker@gmail.com>
2020-04-04 14:09:40 -0500

    added video filtering buttons

--- javascript.js
+++ javascript.js
@@ -1,3 +1,5 @@
+//Navbar
+
 var width = window.innerWidth;
 
 function collapse() {
@@ -11,9 +13,56 @@ if (x.className === "navbar") {
 }
 window.onscroll = function() {
 var currentScrollPos = window.pageYOffset;
-if (currentScrollPos > 60) {
-    document.getElementById("myNavbar").style.boxShadow= "0 3px 3px rgba(0,0,0,0.5)";
-} else {
-    document.getElementById("myNavbar").style.boxShadow= "none"
+    if (currentScrollPos > 60) {
+        document.getElementById("myNavbar").style.boxShadow= "0 3px 3px rgba(0,0,0,0.5)";
+    } else {
+        document.getElementById("myNavbar").style.boxShadow= "none"
+    }
 }
+
+
+
+
+//Filtering Videos
+filterSelection("all")
+function filterSelection(c) {
+var x, i;
+x = document.getElementsByClassName("video");
+if (c == "all") c = "";
+for (i = 0; i < x.length; i++) {
+    removeClass(x[i], "video-show");
+    if (x[i].className.indexOf(c) > -1) addClass(x[i], "video-show");
+}
+}
+
+function addClass(element, name) {
+var i, arr1, arr2;
+arr1 = element.className.split(" ");
+arr2 = name.split(" ");
+for (i = 0; i < arr2.length; i++) {
+    if (arr1.indexOf(arr2[i]) == -1) {element.className += " " + arr2[i];}
+}
+}
+
+function removeClass(element, name) {
+var i, arr1, arr2;
+arr1 = element.className.split(" ");
+arr2 = name.split(" ");
+for (i = 0; i < arr2.length; i++) {
+    while (arr1.indexOf(arr2[i]) > -1) {
+    arr1.splice(arr1.indexOf(arr2[i]), 1);     
+    }
+}
+element.className = arr1.join(" ");
+}
+
+//Add active class to the current button (highlight it)
+var btnContainer = document.getElementById("video-filter-button-container");
+var btns = btnContainer.getElementsByClassName("video-filter-button");
+for (var i = 0; i < btns.length; i++) {
+  btns[i].addEventListener("click", function(){
+    var current = document.getElementsByClassName("vfb-active");
+    current[0].className = current[0].className.replace(" vfb-active", "");
+    this.className += " vfb-active";
+  });
 }
