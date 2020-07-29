e9e19a880398e13cb6c414eef2a89d1beedb7e16
RawBaconOnAStick <rohan.dharwadker@gmail.com>
2020-07-28 20:13:26 -0500

    Added individual products page

--- productpricechange.js
+++ productpricechange.js
@@ -1,23 +1,24 @@
 function updatePrice(){
-	var WNPPS1_Select =  document.getElementById('select-WNPPS1');
-	var WNPPS1_Price = document.getElementById('price-WNPPS1');
-	if (WNPPS1_Select.value == "Black"){
-		WNPPS1_Price.innerHTML = "$6.99";
-		console.log("updated price of WNPPS1 to Black");
-	}
-	else{
-		WNPPS1_Price.innerHTML = "$4.99";
-		console.log("updated price of WNPPS1 to White");
-	}
-
-	var WNPWC_Select =  document.getElementById('select-WNPWC');
-	var WNPWC_Price = document.getElementById('price-WNPWC');
-	if (WNPWC_Select.value == "Black"){
-		WNPWC_Price.innerHTML = "$1.99";
-		console.log("updated price of WNPWC to Black");
-	}
-	else{
-		WNPWC_Price.innerHTML = "$0.99";
-		console.log("updated price of WNPWC to White");
+	try:
+		var WNPPS1_Select =  document.getElementById('select-WNPPS1');
+		var WNPPS1_Price = document.getElementById('price-WNPPS1');
+		if (WNPPS1_Select.value == "Black"){
+			WNPPS1_Price.innerHTML = "$6.99";
+			console.log("updated price of WNPPS1 to Black");
+		}
+		else{
+			WNPPS1_Price.innerHTML = "$4.99";
+			console.log("updated price of WNPPS1 to White");
+		}
+	try:
+		var WNPWC_Select =  document.getElementById('select-WNPWC');
+		var WNPWC_Price = document.getElementById('price-WNPWC');
+		if (WNPWC_Select.value == "Black"){
+			WNPWC_Price.innerHTML = "$1.99";
+			console.log("updated price of WNPWC to Black");
+		}
+		else{
+			WNPWC_Price.innerHTML = "$0.99";
+			console.log("updated price of WNPWC to White");
 	}
 }
